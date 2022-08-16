856d91977456ca929177f18ada17e4106a9db41b
Mentalich <101927817+Mentalich@users.noreply.github.com>
2022-08-16 23:27:55 +0300

    Add files via upload
    imports cleanup

--- TestTask_SelenideUI/src/test/java/pages/GoogleSearchResult.java
+++ TestTask_SelenideUI/src/test/java/pages/GoogleSearchResult.java
@@ -1,20 +1,12 @@
 package pages;
 
-import com.codeborne.selenide.Selectors;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.files.FileFilter;
-import com.codeborne.selenide.selector.*;
-import org.openqa.selenium.By;
 
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.download;
 
 public class GoogleSearchResult {
     private SelenideElement pictureTabButton = $("div.hdtb-mitem:nth-child(2) > a:nth-child(1)");
@@ -22,11 +14,10 @@ public class GoogleSearchResult {
         return pictureTabButton;
     }
 
-    private SelenideElement resultTable = $(".KAlRDb");
     private SelenideElement result = $("div.isv-r:nth-child(4)");
-    public void savePicture() throws IOException, URISyntaxException {
+    private SelenideElement resultTable = $(".KAlRDb");
+        public void savePicture() throws IOException, URISyntaxException {
         result.click();
         download(resultTable.getAttribute("src"));
-
     }
 }
